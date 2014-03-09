d3348642889d6c44f0ec02af7e985877178bb7b2
Sajjad Manal <sajjadmanal24@gmail.com>
2014-03-09 19:15:52 +0530

    Use of Throw and Throws

---
+++ Throw-And-Throws/Throws1.java
@@ -0,0 +1,21 @@
+// Using class Throws..
+
+
+class Throws1
+{
+public static void main(String... s)
+{
+Throws t=new Throws();
+try
+{
+double r=t.divide(10,20);
+System.out.println(r);
+}
+catch(ArithmeticException e)
+{
+System.out.println(e);
+
+
+}
+}
+}
