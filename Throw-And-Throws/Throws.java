e57d4899f6d7ac0221ec9c72161dda056839d240
Sajjad Manal <sajjadmanal24@gmail.com>
2014-03-09 19:13:27 +0530

    Use of Throw and Throws

---
+++ Throw-And-Throws/Throws.java
@@ -0,0 +1,21 @@
+// Use of throws to avoid handler..
+// Made by Developer ,We are using it in class Throws1....
+class Throws
+{
+double divide(double x, double y)throws ArithmeticException 
+{
+
+
+return x/y;
+
+
+
+
+}
+
+
+
+
+
+
+}
