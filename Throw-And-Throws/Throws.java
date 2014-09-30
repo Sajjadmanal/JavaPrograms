9e74f2defe18fa5e08fad6c5ef3356a900e33ca0
Sajjad Manal <sajjadmanal24@gmail.com>
2014-09-30 19:39:35 +0530

    Basic Java Programs

--- Throw-And-Throws/Throws.java
+++ Throw-And-Throws/Throws.java
@@ -1,9 +1,10 @@
-// Use of throws to avoid handler..
+// Use of throws to avoid handler....
 // Made by Developer ,We are using it in class Throws1....
 class Throws
 {
 double divide(double x, double y)throws ArithmeticException 
 {
+
 
 
 return x/y;
